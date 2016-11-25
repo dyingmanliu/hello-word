83052df42ddf728797427d1316846a344a7ceaa6
guangyu.liu <guangyu.liu@chian.zhaogang.com>
2016-11-25 13:59:29 +0800

    commit 4

--- src/main/java/com/zhaogang/test/TestGit.java
+++ src/main/java/com/zhaogang/test/TestGit.java
@@ -8,5 +8,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("commit 1");
         System.out.println("commit 3");
+        System.out.println("commit 4");
+
+
     }
 }
