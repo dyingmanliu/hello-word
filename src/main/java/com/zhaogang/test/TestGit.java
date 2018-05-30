8fa62429afaab2d36199278037df0f0e54c073d7
guangyu.liu <guangyu.liu@chian.zhaogang.com>
2018-05-30 20:57:14 +0800

    test v1.1.0

--- src/main/java/com/zhaogang/test/TestGit.java
+++ src/main/java/com/zhaogang/test/TestGit.java
@@ -4,7 +4,7 @@ package com.zhaogang.test;
  * Created by guangyu.liu on 2016/11/25.
  */
 public class TestGit {
-
+    String i = "test v1.1.0";
     public static void main(String[] args) {
         System.out.println("commit 1");
         System.out.println("commit 3");
